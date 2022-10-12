f72cf64866eab45e47b6657be4aa3d5b1c4fd946
Dimitris Papagiannis <nothingface0@gmail.com>
2022-10-12 12:01:59 +0200

    Added more tests

--- CUDADataFormats/Track/test/trivially_constructible_eric_soa.cpp
+++ CUDADataFormats/Track/test/trivially_constructible_eric_soa.cpp
@@ -3,11 +3,12 @@
 #include <stdint.h>
 #include <assert.h>
 #include "CUDADataFormats/Common/interface/PortableDeviceCollection.h"
-#include "CUDADataFormats/Track/interface/PixelTrackHeterogeneous.h"
+#include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousT_test.h"
+
 using namespace std;
 
 int main() {
-  std::cout << "pixelTrack::TrackSoA trivially constructible: "
+  std::cout << "pixelTrack::TrackSoA with Eric's SoA, trivially constructible: "
             << std::is_trivially_constructible<pixelTrack::TrackSoA>::value << std::endl;
 
   std::cout << "cms::cuda::PortableDeviceCollection<TrackSoAHeterogeneousT_test<>> trivially constructible: "
