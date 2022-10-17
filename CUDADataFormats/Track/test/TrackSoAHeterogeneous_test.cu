1f07ee7a7e36d526ae4751fc156cf12028c061d8
Breno Orzari <breno.orzari@hotmail.com>
2022-10-17 16:12:30 +0200

    Adding successful kernel tests

--- CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cu
+++ CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cu
@@ -1,31 +1,37 @@
-#include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousT_test.h"
-#include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+#include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousT_test2.h"
+
 namespace testTrackSoAHeterogeneousT {
 
-  __global__ void fill(pixelTrack::TrackSoAView tracks, unsigned int soaSize) {
+  __global__ void fill(pixelTrack::TrackSoAView tracks, uint32_t soaSize) {
+    //assert(tracks);
+
     int i = threadIdx.x;
     if (i >= soaSize)
       return;
     tracks[i].pt() = (float)i;
+    tracks[i].eta() = (float)i;
+    tracks[i].chi2() = (float)i;
+    tracks[i].quality() = (uint8_t)i;
+    tracks[i].nLayers() = i%128;
   }
 
-  __global__ void verify(pixelTrack::TrackSoAConstView tracks, unsigned int soaSize) {
+  __global__ void verify(pixelTrack::TrackSoAConstView tracks, uint32_t soaSize) {
+    //assert(tracks);
+
     int i = threadIdx.x;
     if (i >= soaSize)
       return;
-    assert(tracks[i].pt() == (float)i);
+    assert(abs(tracks[i].pt() - (float)i) < .0001);
+    assert(abs(tracks[i].eta() - (float)i) < .0001);
+    assert(abs(tracks[i].chi2() - (float)i) < .0001);
+    assert(tracks[i].quality() == i);
+    assert(tracks[i].nLayers() == i%128);
   }
 
-  void runKernels(pixelTrack::TrackSoAView tracks, unsigned int soaSize) {
+  void runKernels(pixelTrack::TrackSoAView tracks, uint32_t soaSize) {
+    //assert(tracks);
     fill<<<1, 1024>>>(tracks, soaSize);
-    cudaError_t cudaerr = cudaDeviceSynchronize();
-    if (cudaerr != cudaSuccess)
-      printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
-
     verify<<<1, 1024>>>(tracks, soaSize);
-    cudaerr = cudaDeviceSynchronize();
-    if (cudaerr != cudaSuccess)
-      printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
   }
 
-}  // namespace testTrackSoAHeterogeneousT
+}  // namespace testTrackingRecHit2D
