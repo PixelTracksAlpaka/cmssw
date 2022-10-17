b3b82bf22c2c3f7b7b83afed27e0c9ed4a2990ab
Dimitris Papagiannis <nothingface0@gmail.com>
2022-10-17 15:25:49 +0200

    Added Breno's tests

--- CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cpp
+++ CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cpp
@@ -5,7 +5,9 @@
 #include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
 
 namespace testTrackSoAHeterogeneousT {
-  void runKernels(pixelTrack::TrackSoAView tracks, unsigned int soaSize);
+
+  void runKernels(pixelTrack::TrackSoAView tracks, uint32_t soaSize);
+
 }
 
 int main() {
@@ -17,9 +19,16 @@ int main() {
   const auto soaSize = 256;
   // inner scope to deallocate memory before destroying the stream
   {
-    // pixelTrack::TrackSoA tracks;
     TrackSoAHeterogeneousT<soaSize> tracks(stream);
+    auto ret = cms::cuda::make_host_unique<std::byte[]>(tracks.bufferSize(), stream);
     testTrackSoAHeterogeneousT::runKernels(tracks.view(), soaSize);
+    cudaCheck(cudaMemcpy(ret.get(), tracks.buffer().get(),TrackSoAHeterogeneousT_test<>::computeDataSize(soaSize),cudaMemcpyDeviceToHost));
+    TrackSoAHeterogeneousT_test<> tmp_layout(ret.get(),soaSize);
+    TrackSoAHeterogeneousT_test<>::View tmp_view(tmp_layout);
+    std::cout << "pt" << "\t" << "eta" << "\t" <<"chi2" << "\t" << "quality" << "\t" << "nLayers" << std::endl;
+    for(int i = 0; i < soaSize; ++i){
+      std::cout << tmp_view[i].pt() << "\t" << tmp_view[i].eta() << "\t" << tmp_view[i].chi2() << "\t" << (int)tmp_view[i].quality() << "\t" << (int)tmp_view[i].nLayers() << std::endl;
+    }
   }
   cudaCheck(cudaStreamDestroy(stream));
 
