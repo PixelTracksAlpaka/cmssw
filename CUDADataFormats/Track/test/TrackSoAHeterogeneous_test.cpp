f8239610c93e287e2973877c1ee98e955f0b7009
Dimitris Papagiannis <nothingface0@gmail.com>
2022-10-18 15:08:59 +0200

    FINALLY TESTS WORK(?)

--- CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cpp
+++ CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cpp
@@ -1,13 +1,12 @@
 #include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousT_test.h"
-#include "HeterogeneousCore/CUDAUtilities/interface/copyAsync.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/requireDevices.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
-#include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/allocate_device.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/currentDevice.h"
 
 namespace testTrackSoAHeterogeneousT {
 
-  void runKernels(pixelTrack::TrackSoAView tracks, uint32_t soaSize);
-
+  void runKernels(pixelTrack::TrackSoA *tracks, pixelTrack::TrackSoAView tracks_view);
 }
 
 int main() {
@@ -16,19 +15,52 @@ int main() {
   cudaStream_t stream;
   cudaCheck(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
 
-  const auto soaSize = 256;
   // inner scope to deallocate memory before destroying the stream
   {
-    TrackSoAHeterogeneousT<soaSize> tracks(stream);
-    auto ret = cms::cuda::make_host_unique<std::byte[]>(tracks.bufferSize(), stream);
-    testTrackSoAHeterogeneousT::runKernels(tracks.view(), soaSize);
-    cudaCheck(cudaMemcpy(ret.get(), tracks.buffer().get(),TrackSoAHeterogeneousT_test<>::computeDataSize(soaSize),cudaMemcpyDeviceToHost));
-    TrackSoAHeterogeneousT_test<> tmp_layout(ret.get(),soaSize);
+    // Instantiate tracks on host. Portabledevicecollection allocates
+    // SoA on device automatically.
+    int dev = cms::cuda::currentDevice();
+    pixelTrack::TrackSoA tracks_h(stream);
+
+    // Make a copy of tracks_h to device, so that we can
+    // modify hitIndices.
+    void *mem = cms::cuda::allocate_device(dev, sizeof(pixelTrack::TrackSoA), stream);
+    cudaCheck(cudaMemcpy(mem, &tracks_h, sizeof(pixelTrack::TrackSoA), cudaMemcpyHostToDevice));
+
+    // Run the tests
+    pixelTrack::TrackSoA *tracks_d = reinterpret_cast<pixelTrack::TrackSoA *>(mem);
+    testTrackSoAHeterogeneousT::runKernels(tracks_d, tracks_h.view());
+
+    // Copy SoA data back to host
+    auto ret = cms::cuda::make_host_unique<std::byte[]>(tracks_h.bufferSize(), stream);
+    cudaCheck(cudaMemcpy(ret.get(),
+                         tracks_h.buffer().get(),
+                         TrackSoAHeterogeneousT_test<>::computeDataSize(tracks_h.stride()),
+                         cudaMemcpyDeviceToHost));
+
+    // Copy tracks_d back to tracks_h
+    cudaCheck(cudaMemcpy(&tracks_h, mem, sizeof(pixelTrack::TrackSoA), cudaMemcpyDeviceToHost));
+
+    // Create a view to access the copied data
+    TrackSoAHeterogeneousT_test<> tmp_layout(ret.get(), tracks_h.stride());
     TrackSoAHeterogeneousT_test<>::View tmp_view(tmp_layout);
-    std::cout << "pt" << "\t" << "eta" << "\t" <<"chi2" << "\t" << "quality" << "\t" << "nLayers" << std::endl;
-    for(int i = 0; i < soaSize; ++i){
-      std::cout << tmp_view[i].pt() << "\t" << tmp_view[i].eta() << "\t" << tmp_view[i].chi2() << "\t" << (int)tmp_view[i].quality() << "\t" << (int)tmp_view[i].nLayers() << std::endl;
+    std::cout << "pt"
+              << "\t"
+              << "eta"
+              << "\t"
+              << "chi2"
+              << "\t"
+              << "quality"
+              << "\t"
+              << "nLayers"
+              << "\t"
+              << "hitIndices off" << std::endl;
+    for (int i = 0; i < tracks_h.stride(); ++i) {
+      std::cout << tmp_view[i].pt() << "\t" << tmp_view[i].eta() << "\t" << tmp_view[i].chi2() << "\t"
+                << (int)tmp_view[i].quality() << "\t" << (int)tmp_view[i].nLayers() << "\t"
+                << tracks_h.hitIndices.off[i] << std::endl;
     }
+    cudaCheck(cudaFree(mem));
   }
   cudaCheck(cudaStreamDestroy(stream));
 
--- CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cu
+++ CUDADataFormats/Track/test/TrackSoAHeterogeneous_test.cu
@@ -1,37 +1,43 @@
 #include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousT_test.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/OneToManyAssoc.h"
 
 namespace testTrackSoAHeterogeneousT {
 
-  __global__ void fill(pixelTrack::TrackSoAView tracks, uint32_t soaSize) {
-    //assert(tracks);
+  __global__ void fill(pixelTrack::TrackSoA* __restrict__ tracks, pixelTrack::TrackSoAView tracks_view) {
+    assert(tracks);
 
     int i = threadIdx.x;
-    if (i >= soaSize)
-      return;
-    tracks[i].pt() = (float)i;
-    tracks[i].eta() = (float)i;
-    tracks[i].chi2() = (float)i;
-    tracks[i].quality() = (uint8_t)i;
-    tracks[i].nLayers() = i % 128;
+    for (int j = i; j < tracks->stride(); j += blockDim.x) {
+      tracks_view[j].pt() = (float)j;
+      tracks_view[j].eta() = (float)j;
+      tracks_view[j].chi2() = (float)j;
+      tracks_view[j].quality() = (uint8_t)j % 256;
+      tracks_view[j].nLayers() = j % 128;
+      tracks->hitIndices.off[j] = j;
+    }
   }
 
-  __global__ void verify(pixelTrack::TrackSoAConstView tracks, uint32_t soaSize) {
-    //assert(tracks);
+  __global__ void verify(pixelTrack::TrackSoA* const __restrict__ tracks, pixelTrack::TrackSoAConstView tracks_view) {
+    assert(tracks);
 
     int i = threadIdx.x;
-    if (i >= soaSize)
-      return;
-    assert(abs(tracks[i].pt() - (float)i) < .0001);
-    assert(abs(tracks[i].eta() - (float)i) < .0001);
-    assert(abs(tracks[i].chi2() - (float)i) < .0001);
-    assert(tracks[i].quality() == i);
-    assert(tracks[i].nLayers() == i % 128);
+    if (i == 0) {
+      printf("Stride: %d, block dims: %d\n", tracks->stride(), blockDim.x);
+    }
+    for (int j = i; j < tracks->stride(); j += blockDim.x) {
+      assert(abs(tracks_view[j].pt() - (float)j) < .0001);
+      assert(abs(tracks_view[j].eta() - (float)j) < .0001);
+      assert(abs(tracks_view[j].chi2() - (float)j) < .0001);
+      assert(tracks_view[j].quality() == j % 256);
+      assert(tracks_view[j].nLayers() == j % 128);
+      assert(tracks->hitIndices.off[j] == j);
+    }
   }
 
-  void runKernels(pixelTrack::TrackSoAView tracks, uint32_t soaSize) {
-    //assert(tracks);
-    fill<<<1, 1024>>>(tracks, soaSize);
-    verify<<<1, 1024>>>(tracks, soaSize);
+  void runKernels(pixelTrack::TrackSoA* tracks, pixelTrack::TrackSoAView tracks_view) {
+    assert(tracks);
+    fill<<<1, 1024>>>(tracks, tracks_view);
+    verify<<<1, 1024>>>(tracks, tracks_view);
   }
 
-}  // namespace testTrackingRecHit2D
+}  // namespace testTrackSoAHeterogeneousT
