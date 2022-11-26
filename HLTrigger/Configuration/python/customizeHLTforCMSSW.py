68aa1cc2581e4ff3a94d29b898387a044ad6a0cf
AdrianoDee <adriano.diflorio@ba.infn.it>
2022-11-26 16:25:07 +0100

    Pixel Tracks and Vertices Portable Data Formats
    Co-authored-by: Breno Orzari <breno.orzari@hotmail.com>
    Co-authored-by: Dimitris Papagiannis <nothingface0@gmail.com>
    Co-authored-by: Dimitris Papagiannis <d.papag.a@gmail.com>
    Co-authored-by: borzari <40498845+borzari@users.noreply.github.com>

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -248,6 +248,7 @@ def customizeHLTfor40264(process):
 
     return process
 
+
 def customizeHLTfor40334(process):
 
   for producer in producers_by_type(process, 'PSMonitor'):
@@ -259,6 +260,14 @@ def customizeHLTfor40334(process):
   return process
 
 
+def customizeHLTfor40465(process):
+    try:
+        process.hltSiPixelRecHitsSoA.cpu.hltSiPixelRecHitsFromLegacy[0].type = 'pixelTopologyPhase1TrackingRecHitSoAHost'
+    except:
+        pass
+    return process
+
+
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
 
@@ -266,9 +275,10 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
 
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
- 
+
     process = customizeHLTfor38761(process)
     process = customizeHLTfor40264(process)
     process = customizeHLTfor40334(process)
+    process = customizeHLTfor40465(process)
 
     return process
