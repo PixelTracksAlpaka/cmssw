93c1e9d8abd43aacc7faaa890121faa864a16cd2
Amitav Mitra <64038220+ammitra@users.noreply.github.com>
2023-09-21 12:30:57 -0400

    explanation for ESProduct type lookup data

--- RecoLocalTracker/SiPixelRecHits/src/PixelCPEFastParams.cc
+++ RecoLocalTracker/SiPixelRecHits/src/PixelCPEFastParams.cc
@@ -2,6 +2,8 @@
 #include "RecoLocalTracker/SiPixelRecHits/interface/PixelCPEFastParams.h"
 #include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
 
+// Compilation of this file informs framework about the production of the CPEFastParams ESProduct through type lookup data
+
 using PixelCPEFastParamsPhase1 = pixelCPEforDevice::PixelCPEFastParams<alpaka_common::DevHost, pixelTopology::Phase1>;
 using PixelCPEFastParamsPhase2 = pixelCPEforDevice::PixelCPEFastParams<alpaka_common::DevHost, pixelTopology::Phase2>;
 
