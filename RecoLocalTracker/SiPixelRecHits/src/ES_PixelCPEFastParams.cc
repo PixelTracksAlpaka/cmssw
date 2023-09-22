05527871f549eb505f660d5ad39d5c3370de928d
Matej Roguljic <matej.roguljic@gmail.com>
2023-09-22 16:26:06 +0200

    ES CPEFastParams documentation

--- RecoLocalTracker/SiPixelRecHits/src/ES_PixelCPEFastParams.cc
+++ RecoLocalTracker/SiPixelRecHits/src/ES_PixelCPEFastParams.cc
@@ -2,6 +2,8 @@
 #include "FWCore/Utilities/interface/typelookup.h"
 #include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
 
+// Compilation of this file informs framework about the production of the CPEFastParamsHost ESProduct through type lookup data
+
 using PixelCPEFastParamsHostPhase1 = PixelCPEFastParamsHost<pixelTopology::Phase1>;
 using PixelCPEFastParamsHostPhase2 = PixelCPEFastParamsHost<pixelTopology::Phase2>;
 
--- RecoLocalTracker/SiPixelRecHits/src/PixelCPEFastParams.cc
+++ RecoLocalTracker/SiPixelRecHits/src/PixelCPEFastParams.cc
@@ -2,6 +2,8 @@
 #include "RecoLocalTracker/SiPixelRecHits/interface/PixelCPEFastParamsDevice.h"
 #include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
 
+//Compilation of this file informs framework about the production of the CPEFastParams ESProduct through type lookup data
+
 using PixelCPEFastParamsPhase1 = PixelCPEFastParamsDevice<alpaka_common::DevHost, pixelTopology::Phase1>;
 using PixelCPEFastParamsPhase2 = PixelCPEFastParamsDevice<alpaka_common::DevHost, pixelTopology::Phase2>;
 
